8ac6c535131199faf82a8bbb83a5c7b7a4a2e205
nana <118910416+jianc0re@users.noreply.github.com>
2024-07-26 12:03:37 -0300

    c1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 69;
 console.log('numeroSecreto')
 let chute = prompt('Escolha um número entre 0 e 69');
 
