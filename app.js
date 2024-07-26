e37056a3c12af53a959a8c3c02145d3b615ce9b0
nana <118910416+jianc0re@users.noreply.github.com>
2024-07-26 12:01:55 -0300

    c1

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log('numeroSecreto')
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute = prompt('Escolha um número entre 0 e 69');
 
 // se chute for igual ao número secreto
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (5)');
+alert('Isso ai! taporra (69)');
 } else {
-alert('Você errou :(')
+alert('vai besta :(')
 }
